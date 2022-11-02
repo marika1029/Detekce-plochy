d66ab8566b58729126d509c9418a47e7458f1ba6
marika1029 <113998022+marika1029@users.noreply.github.com>
2022-11-02 17:24:53 +0100

    varianta 2
    Vyřešeno vypisování souřadnic při kliknutí uživatelem.
    Vyřešeno vytvoření výřezu z původního snímku.
    Nutno dodělat dotaz na uživatele jestli s výřezem souhlasí nebo ho chce předělat (vrácení do fáze vypisování souřadnic) + bylo by fajn ověřit fungování tvoření výřezu obrázku

---
+++ main.cpp.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+Mat img, img_gray, img_blur, obd;
+
+static void onClick(int event, int x, int y, int flags, void* param) //bude uživateli ukazovat jaké souřadnice má bod na kterém je myš
+{
+    if (event==1)
+        {
+            cout << "souradnice x = " << x << "; souradnice y = " << y << endl;
+        }
+}
+
+int main ()
+{
+    //načtení obrázku
+    img = imread("obrazek.bmp");
+    if (img.empty())
+    {
+        cout << "Soubor nenalezen." << endl;
+        return -1;
+    }
+
+    cvtColor(img, img_gray, COLOR_BGR2GRAY); //převede do odstínů šedé
+    GaussianBlur(img_gray, img_blur, Size(3, 3), 0); //vzhlazení pomocí blur, medianBlur i bilateralFilter ničí hrany (parametry 3.3 zvoleny aby byla zachována co nejvyšší přesnost)
+
+    //zobrazí obrázky po převedení do spektra šedé a po aplikaci vyhlazení
+    imshow("Obrázek černobíle", img_gray);
+    imshow("Gaussovo vyhlazování", img_blur);
+    waitKey(0);
+
+    //výběr oblasti o kterou se zajímáme (zobrazí souřadnice bodu na který klikne uživatel miší)
+    imshow("Označte oblast hledané plochy:", img_blur);
+    setMouseCallback("Označte oblast hledané plochy:", onClick, 0);
+    waitKey(0);
+
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+
+    cout << "Zapište souřadnici x prvního rohového bodu: "; cin >> x1;
+    cout << "Zapište souřadnici y prvního rohového bodu: "; cin >> y1;
+    cout << "Zapište souřadnici x druhého rohového bodu: "; cin >> x2;
+    cout << "Zapište souřadnici y druhého rohového bodu: "; cin >> y2;
+
+    //vytvoření obdélníku s hledanou oblastí
+
+    obd = img(Rect(x1,y1,x2,y2));
+    imshow("Vyznačená obdélníková oblast", obd);
+    waitKey(0);
+
+    //dále aplikace algoritmu na hledání hran v danném rozsahu
+    //kontrola uzavřenosti oblasti (případně návrat do předchozího kroku s nižším kontrasten...bylo by možné využít toho co měli v projektu naši předchůdci a nechat uživatele mrknout kdy už to najde celou oblast a pak ho nechat zadat požadovaný kontrast případně umožnit změnu hledaného kontrastu jen v urřité podoblasti)
+    //zobrazení nalezené oblasti
+    //výpočet plochy této oblasti
+    return 0;
+}
